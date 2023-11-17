52dca673b56446a148b742c25551253135c23daa
atmani samira <erp.atmanis@gmail.com>
2023-11-17 22:36:45 +0100

    mettre les images dans assets

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,35 +1,13 @@
-// Récupération des phot depuis le fichier JSON
-const reponse = await fetch("/data/photographers.json");
-const fichier = await reponse.json();
-
 async function getPhotographers() {
+  const reponse = await fetch("data/photographers.json");
+  const photographers = await reponse.json();
+  //const data = await data.photographers;
+
   // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
   // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-  let photographers = [
-    {
-      name: "Ma data test",
-      id: 1,
-      city: "Paris",
-      country: "France",
-      tagline: "Ceci est ma data test",
-      price: 400,
-      portrait: "account.png",
-    },
-    {
-      name: "Autre data test",
-      id: 2,
-      city: "Londres",
-      country: "UK",
-      tagline: "Ceci est ma data test 2",
-      price: 500,
-      portrait: "account.png",
-    },
-  ];
-
+  console.log(photographers);
   // et bien retourner le tableau photographers seulement une fois récupéré
-  return {
-    photographers: [...photographers, ...photographers, ...photographers],
-  };
+  return photographers;
 }
 
 async function displayData(photographers) {
